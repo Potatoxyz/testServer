725193dd133915691a02d367673ce58967483d98
陈晖 <512006634@qq.com>
2018-08-15 15:54:26 +0800

    upload

--- routes/test.js
+++ routes/test.js
@@ -1,7 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.send({ success: 'true' });
@@ -14,6 +17,27 @@ router.post('/goodCode', function (req, res, next) {
     var r = { IsSuccess: true, Content: [], ErrorMessage: '' };
     res.send(r);
     res.status(200).end();
+});
+var upload = multer({ dest: 'uploads/' }).single('uploadForm'); //必须填写字段名
+var uploadPath = path.resolve(__dirname, '../uploads');
+router.post('/upload', function (req, res, next) {
+    upload(req, res, function (err) {
+        if (err) {
+            res.status(500).end();
+            return;
+        }
+        console.log(req.file);
+        var file = req.file;
+        var oldPath = uploadPath + '/' + file.filename;
+        var newPath = uploadPath + '/' + file.originalname;
+        fs.rename(oldPath, newPath, function (err) {
+            if (err)
+                console.error(err);
+        });
+        var r = { IsSuccess: true, Content: [], ErrorMessage: '' };
+        res.send(r);
+        res.status(200).end();
+    });
 });
 module.exports = router;
 var resModel = (function () {
--- routes/test.ts
+++ routes/test.ts
@@ -1,5 +1,8 @@
 var express = require('express');
+var multer  = require('multer')
 var router = express.Router();
+var fs=require('fs');
+var path=require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +16,29 @@ router.post('/goodCode', function(req, res, next) {
     let r:resModel={IsSuccess:true,Content:[],ErrorMessage:''};
     res.send(r);
     res.status(200).end();
+});
+
+var upload = multer({ dest: 'uploads/' }).single('uploadForm');//必须填写字段名
+var uploadPath=path.resolve(__dirname,'../uploads');
+router.post('/upload',function(req, res, next) {
+    upload(req, res, function (err) {
+        if (err) {
+            res.status(500).end();
+            return;
+        }
+        console.log(req.file);
+        var file=req.file;
+        var oldPath=uploadPath+'/'+file.filename;
+        var newPath=uploadPath+'/'+file.originalname;
+        fs.rename(oldPath,newPath,err=>{
+            if(err)
+                console.error(err);
+        });
+        let r:resModel={IsSuccess:true,Content:[],ErrorMessage:''};
+        res.send(r);
+        res.status(200).end();
+    })
+
 });
 module.exports = router;
 export class resModel{
