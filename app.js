aeded66a2e0663dee1b6d5b02a2cded72e2118f5
陈晖 <512006634@qq.com>
2018-08-29 18:32:39 +0800

    使用新模板

--- app.js
+++ app.js
@@ -21,8 +21,11 @@ app.all('*', function(req, res, next) {
     else  {next()}
 });
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'myView'));
+
+//更换模板引擎
+// app.set('view engine', 'jade');
+app.engine('html', require('express-art-template'))
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
